bcf2462d490c496eb61e5487690689a18b7d837e
Haidarali Masu <haidaralimasu123@gmail.com>
2022-08-18 11:04:07 +0530

    initial commit

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const infuraId = "d014af161a4b4ffbaa358366e232e2c8";
-export const cryptoHandsAddress = "0x68141098bf63B156B8cB26Ced499932FDD0fB4b5";
-export const rpsAddress = "0xE5303470883A2490677D4A3642Bd4DB726163e03";
+export const cryptoHandsAddress = "0x9442D33E906Ce66de7e5653346bA42Df326673D6";
+export const rpsAddress = "0x2958Ead36E9723C10c15333ef0F5f5F57F023c65";
 export const graphEndpoint =
   "https://api.thegraph.com/subgraphs/name/haidaralimasu/crypto-hands-subgraph";
 
